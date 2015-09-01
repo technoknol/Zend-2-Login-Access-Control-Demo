3c3848a5bd93f3fd39808c5695282418315a3a9a
JVerstry <jverstry@gmail.com>
2015-09-01 09:10:15 +0200

    Removing Template module

--- config/application.config.php
+++ config/application.config.php
@@ -8,8 +8,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Main',
-        'Template'
+        'Main'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
